437959b9f21cf0bcc4d6691e21495e8dffd2db58
Wheatley <shestyara10@gmail.com>
2023-11-28 11:50:43 +0300

    1.0

---
+++ CasinoGuyCash/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoGuyCash
+{
+    internal static class Helper
+    {
+        static public string Capitalize(this string upperStr) {
+            string? result;
+            if (upperStr == "") { return "Noname"; }
+            else {
+                result = upperStr[0].ToString().ToUpper() + upperStr.Remove(0, 1);
+                return result;
+            }
+        }
+
+    }
+}
--- CasinoGuyCash/Program.cs
+++ CasinoGuyCash/Program.cs
@@ -1,51 +1,168 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CasinoGuyCash;
 using static Guy;
 
+Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("Name Player:");
-string player = Console.ReadLine().ToString();
-Console.WriteLine("Count " + player + ":");
-string coconut = Console.ReadLine();
-Random random = new Random();
-double rndmOdds = random.NextDouble();
+string? player = Console.ReadLine().ToString().Capitalize();
 
-    if (coconut == "") return;
-
+Random random = new Random(DateTime.Now.Millisecond);
+    
     for (bool a = false; a == false;)
     {
-        if (int.TryParse(coconut, out int amount))
+    string coconut = ""; 
+    
+        for (; coconut == ""; coconut = Console.ReadLine())
         {
-            Guy Player = new() { Cash = amount, Name = player };
-            
-            Console.WriteLine(rndmOdds);
-            
-            
-            Console.WriteLine("Hello " + Player.Name + ", your cash count is " + Player.Cash);
-        Console.WriteLine("your odds (0.1 - 0.9):");
-        string odds = Console.ReadLine();
-        for (bool b = false; b == false;)
-            if (!double.TryParse(odds, out double result))
-            {
-            Console.WriteLine("NaD");
-            }
-        else
-        {
-
+            Console.Write("How much money ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(player);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" have?: ");
         }
-            Console.Write("How much do you give the cash: ");
-        Console.Write();
+    if (coconut.Length > 7) coconut = coconut.Remove(8);
 
+    if (int.TryParse(coconut, out int cash))
+    {
+        Guy Player = new() { Cash = cash, Name = player };
 
-        string whichGuy;
-            Guy who, count;
+        Console.Write("Hello ");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write(Player.Name);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(", your cash count is ");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write( Player.Cash); 
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(", okay");
         
-        a = true;
+        
+        for (bool b = false; b == false;)
+        {
+            Console.WriteLine("Enter your odds (at 1 is easy - to 9 is hard):");
+            string? odds = Console.ReadLine();
+
+            if (int.TryParse(odds, out int easyHard))
+            {
+
+                if (easyHard >= 1 && easyHard <= 9)
+                {
+                    Console.WriteLine("Your odds is: " + easyHard);
+                    double dealOdds = easyHard / 10.0;
+
+                    for (bool c = false; c == false;)
+                    {
+                        Console.Write("Your cash count is: ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(Player.Cash);
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        if (Player.Cash <= 0)
+                        {
+                            Console.Write("Casino ");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("win");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(", congratulations!");
+
+                            return;
+                        };
+                        Console.Write("How much do you give the cash (or a nothing to exit and 'c' to change odds) : ");
+                        string deal = Console.ReadLine();
+
+                        
+                        if (deal == "") return;
+                        if (deal == null) return;
+                        if (deal.Length > 7) deal = deal.Remove(8);
+
+                        if (int.TryParse(deal, out int dealCash) && dealCash >= 0)
+                        {
+
+                            if (dealCash > Player.Cash)
+                            {
+                                Console.Write("YourMaxCount is: ");
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.Write(Player.Cash);
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.Write(", and you deal it");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine(" , it's (f)all in");
+                                dealCash = Player.Cash;
+                            }
+
+                            double rndmOdds = random.NextDouble();
+                            rndmOdds = Math.Round(rndmOdds, 1);
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write(Player.Name);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(" Value chance " + dealOdds);
+                            Console.WriteLine("Casino random chance " + rndmOdds);
+
+                            if (dealOdds <= rndmOdds)
+                            {
+
+                                //double luck = 1.0 - dealOdds;
+
+                                //int win = Convert.ToInt32(luck);
+                                Player.Cash += dealCash * easyHard / 10;
+                                Console.ForegroundColor = ConsoleColor.Magenta;
+                                Console.WriteLine("You Win!");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                            }
+                            else
+                            {
+                                Player.Cash -= dealCash;
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("You Lose!");
+                                Console.ForegroundColor = ConsoleColor.Green;
+
+
+                            }
+                        }
+
+
+                        else if (deal == "c") { c = true; }
+
+                        else
+                        {
+
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.Write("Nan,");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(" give me true natural number pls");
+                            Console.ForegroundColor = ConsoleColor.Green;
+
+
+                        }
+                        // c = true;
+                    }
+
+
+                }
+                else if (easyHard <= 0)
+                {
+                    Console.WriteLine("So easy");
+                }
+                else
+                {
+                    Console.WriteLine("So hardly");
+                }
+            }
+            else
+            {
+                Console.WriteLine("char not num");
+
+            }
+            //b = true;
         }
+        
+        
+    }
         else
         {
-            Console.WriteLine("Please enter an amount (or a nofing to exit).");
-            string nChar = Console.ReadLine();
-            if (nChar == "") return;
+            Console.WriteLine("Please enter any number.");
         }
+   // a = true;
     }
 
     
